c76d66008c80b79d5f359b9744135e019fc8acba
Kis Tamás <kistamas00@freemail.hu>
2018-08-30 21:55:41 +0200

    Initial spring boot application

---
+++ src/main/java/com/kistamas/dev/demo/currencycounter/CurrencyCounterApplication.java
@@ -0,0 +1,12 @@
+package com.kistamas.dev.demo.currencycounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCounterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kistamas/dev/demo/currencycounter/CurrencyCounterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kistamas.dev.demo.currencycounter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyCounterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
